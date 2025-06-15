1670af0ac8e2700ac9de302b719f562b8d7c9a7d
alan-polk <56047663+alan-polk@users.noreply.github.com>
2025-06-14 20:26:19 -0500

    add back human turn logic

--- opendbc_repo/opendbc/car/ford/carcontroller.py
+++ opendbc_repo/opendbc/car/ford/carcontroller.py
@@ -520,17 +520,19 @@ class CarController(CarControllerBase):
           desired_curvature_rate = 0.0
 
 
+        # Determine if a human is making a turn and trap the valueAdd commentMore actions
+        # if a human turn is active, reset steering to prevent windup
+        if steeringPressed and abs(steeringAngleDeg_PV) > 45:
+          self.human_turn = True
+        else:
+          self.human_turn = False
+        
         # Determine when to reset steering
         if ((self.human_turn) and self.enable_human_turn_detection) or (CS.out.vEgoRaw < 0.1):
           reset_steering = 1
         else:
           reset_steering = 0
 
-        # Determine when to reset steering
-        if (self.human_turn) and self.enable_human_turn_detection:
-          reset_steering = 1
-        else:
-          reset_steering = 0
 
         # reset steering by setting all values to 0 and ramp_type to immediate
         if reset_steering == 1:
